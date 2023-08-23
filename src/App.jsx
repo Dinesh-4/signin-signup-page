8ba107fb8a38604b163904c6f8699f91d39bc5b1
Dinesh-4 <dinesh46.mv@gmail.com>
2023-08-23 12:19:51 +0530

    Form field both signIn and SignUp

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,53 @@
-import './App.css'
+import "./index.css";
+import { FaFacebookF, FaGoogle, FaTwitter } from "react-icons/fa";
 
 function App() {
-
   return (
-    <div>Hello World</div>
-  )
+    <div className="container right-panel-active">
+      <div className="form-container sign-up-container">
+        <form action="">
+          <h1>Create Account</h1>
+          <div className="social-container">
+            <a href="#" className="social">
+              <FaFacebookF />
+            </a>
+            <a href="#" className="social">
+              <FaGoogle />
+            </a>
+            <a href="#" className="social">
+              <FaTwitter />
+            </a>
+          </div>
+          <span>or use your email for registration</span>
+          <input type="text" name="name" placeholder="Name" />
+          <input type="email" name="email" placeholder="Email" />
+          <input type="password" name="password" placeholder="Password" />
+          <button>SignUp</button>
+        </form>
+      </div>
+      <div className="form-container sign-in-container">
+        <form action="#">
+          <h1>Sign In</h1>
+          <div className="social-container">
+            <a href="#" className="social">
+              <FaFacebookF />
+            </a>
+            <a href="#" className="social">
+              <FaGoogle />
+            </a>
+            <a href="#" className="social">
+              <FaTwitter />
+            </a>
+          </div>
+          <span>or use your account</span>
+          <input type="email" name="email" placeholder="Email" />
+          <input type="password" name="password" placeholder="Password" />
+          <a href="#">Forgot Your Password</a>
+          <button>Sign In</button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
