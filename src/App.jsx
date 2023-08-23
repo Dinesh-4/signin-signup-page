8847a70505b820400e22b4f0c813cb56c7522835
Dinesh-4 <dinesh46.mv@gmail.com>
2023-08-23 12:53:02 +0530

    html fix

--- src/App.jsx
+++ src/App.jsx
@@ -61,7 +61,7 @@ function App() {
             <p>
               To keep connected with us please login with your personal info
             </p>
-            <button className="ghost" onClick={toggleForm}>SignIn</button>
+            <button className="ghost" onClick={toggleForm}>Sign In</button>
           </div>
           <div className="overlay-panel overlay-right">
             <h1>Hello, Friend!</h1>
